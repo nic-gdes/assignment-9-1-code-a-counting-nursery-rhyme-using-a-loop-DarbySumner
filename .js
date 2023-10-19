c000057c5af848d9a82e24e8705b57ea28855811
Darby <dssumner@nic.edu>
2023-10-19 14:41:43 -0700

    Nursery Rhyme Loop
    defined count  as 4 and added while count to be less than 5. Input lyrics into console.log and added count to equal count plus 1.

---
+++ .js
@@ -0,0 +1,12 @@
+let count = 4;
+
+while (count < 5) {
+    
+    console.log("Baa, baa, black sheep, Have you any wool?");
+    console.log("Yes sir, yes sir, Three bags full.");
+    console.log("One for the master, One for the Dame,");
+    console.log("And one for the little boy Who lives down the lane.");
+
+        count = count + 1;
+    
+}
