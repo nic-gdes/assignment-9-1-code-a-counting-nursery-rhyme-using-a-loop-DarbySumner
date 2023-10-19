d68c905f40c5f534f06ace63792d79fca2632ca3
Darby <dssumner@nic.edu>
2023-10-19 15:25:38 -0700

    Fixing it to display the song lyrics multiple times

--- .js
+++ .js
@@ -1,12 +1,15 @@
-let count = 4;
+let count = 1;
 
 while (count < 5) {
     
     console.log("Baa, baa, black sheep, Have you any wool?");
+    console.log(count);
     console.log("Yes sir, yes sir, Three bags full.");
+    console.log(count);
     console.log("One for the master, One for the Dame,");
+    console.log(count);
     console.log("And one for the little boy Who lives down the lane.");
-
-        count = count + 1;
+    console.log(count);
+    count = count + 1;
     
 }
